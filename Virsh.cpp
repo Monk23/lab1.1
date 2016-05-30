c457963b464632b3b183104f29dd6cebe067776c
Monk23 <head734@ukr.net>
2016-05-30 08:43:45 +0300

    Virsh

---
+++ Virsh.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv) 
+{
+	printf("Spent the night in formation, \n") ;
+	
+	printf("To the battle we marched in the dawn... \n") ;
+	
+	printf("We were ready to die for our king \n") ;
+	
+	printf("On the fields of Breitenfeld.\n") ;
+	
+	printf("On September the seventh,\n") ;
+	
+	printf("We filled their hearts with fear\n") ;
+	
+	printf("Seven times they attacked on that day\n") ;
+	
+	printf("Seven times they retreated \n") ;
+	
+	system("pause") ;
+	return 0 ;
+}
