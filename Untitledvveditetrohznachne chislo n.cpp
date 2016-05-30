c991d0b275694bcdf1548c036d0ab22c864e07c6
Monk23 <head734@ukr.net>
2016-05-30 08:25:58 +0300

    trohznachne chislo

---
+++ Untitledvveditetrohznachne chislo n.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+{
+	int n ;
+	printf("Vvedite tryohznachne chislo n:") ;
+	scanf("%d", &n) ;
+	printf("%o\n", n) ;
+	printf("%x\n", n) ;
+	printf("%d\n", n) ;
+
+	system("pause") ;
+	
+	return 0;
+	
+}
